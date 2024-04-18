1f6545e3538df24d679437360b329c9566939988
22222sun <125752638+22222sun@users.noreply.github.com>
2024-04-18 10:56:15 +0800

    Create index.php

---
+++ php2024midterm/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+
+<form action="check.php" method="post">
+<p>高大資管論文投稿系統<p>
+請選擇您的角色:
+<select name="uRole">
+<option value="chair">Chair
+<option value="reviewer">Reviewer
+<option value="author">Author
+</select>
+<br/>
+姓名:<input type="text" name="uId"><br/>
+密碼:<input type="password" name="uPWD" required><br/><br/>
+<input type="submit" value="提交">
+
+</form>
+
+</html>
