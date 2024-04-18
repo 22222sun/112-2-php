d83bf3e3d5b8f483289f464fd02d7ea9dfc9552e
22222sun <125752638+22222sun@users.noreply.github.com>
2024-04-18 10:58:23 +0800

    Create author.php

---
+++ php2024midterm/author.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<meta charset="utf-8">
+<a href="logout.php"><button>登出</button></a>
+</head>
+
+<form action="showpaper.php" method="post">
+<p>Author您好，歡迎進入論文投稿網頁<p>
+論文標題:<br/>
+<input type="text" name="sHead"><br/>
+作者姓名:<input type="text" name="sName"><br/>
+作者Email:<input type="text" name="sEmail"><br/>
+論文摘要:
+<textarea name="sComment" rows="10" cols="25" >
+</textarea>
+<br/>
+<input type="submit" value="提交">
+</form>
